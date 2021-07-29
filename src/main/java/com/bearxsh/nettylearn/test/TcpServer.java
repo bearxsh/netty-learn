a46da48b21ada423a89c3ff22399f2c6353d5821
Bearxsh <bearxsh@163.com>
2021-07-29 16:03:55 +0800

    udpate

--- src/main/java/com/bearxsh/nettylearn/test/TcpServer.java
+++ src/main/java/com/bearxsh/nettylearn/test/TcpServer.java
@@ -13,20 +13,25 @@ public class TcpServer {
     public static void main(String[] args) throws InterruptedException {
         NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-        ServerBootstrap serverBootstrap = new ServerBootstrap();
-        serverBootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 256)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-                        ChannelPipeline pipeline = ch.pipeline();
-                        pipeline.addLast(new MyServerHandler());
-                    }
-                })
-                .childOption(ChannelOption.SO_KEEPALIVE, true);
-        ChannelFuture channelFuture = serverBootstrap.bind(9001).sync();
-        System.out.println("服务已启动...");
-        channelFuture.channel().closeFuture().sync();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 256)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new MyServerHandler());
+                        }
+                    })
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture channelFuture = serverBootstrap.bind(9001).sync();
+            System.out.println("服务已启动...");
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
     }
 }
